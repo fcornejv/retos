49397fbac103ae5b816553eb096b48f58a647657
fcornejv <frank.pcv@gmail.com>
2021-07-30 23:08:26 -0500

    Update reto1.py

--- BackEnd-Retos/Semana01/reto1.py
+++ BackEnd-Retos/Semana01/reto1.py
@@ -3,13 +3,11 @@
 # 2. Si es menor de edad que indique que dependiendo de la hora (si es mas de las 6pm) debe ir a dormir y si no hacer la tarea.
 # 3. Si es mayor de edad que indique que no esta obligado a hacer nada.
 
-#Importar referencia:
 import datetime
 
 name = input("Ingrese su nombre: ")
 age = int(input("Ingrese su edad: "))
 
-#Establecer condición - Validar edad:
 if age < 18:
   if datetime.datetime.now().strftime('%H:%M:%S') > '18:00:00':
     print("¡", name, " Debes ir a dormir")
